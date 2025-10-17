889e3450e471be85e724453fee6a47f6a4e1a388
POPASMALINOIS <j.m.p.garrido87@outlook.es>
2025-10-18 01:52:19 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
-// Publicado en https://popasmalinois.github.io/plmeco-docks/
-// Para Project Pages, la base debe ser '/<NOMBRE_REPO>/'
+// En esta rama (Docker/Nginx) servimos en raíz:
 export default defineConfig({
   plugins: [react()],
-  base: '/plmeco-docks/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(process.cwd(), 'src'),
