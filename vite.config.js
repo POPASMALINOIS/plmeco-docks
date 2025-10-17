c65737449ad94bd5957486b74f65dd2bd2f43e8f
POPASMALINOIS <j.m.p.garrido87@outlook.es>
2025-10-17 03:43:26 +0200

    Refactor vite.config.js to use ES module syntax

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-// vite.config.ts
-require { defineConfig } from 'vite'
-require react from '@vitejs/plugin-react'
-require path from 'node:path'
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 // Publicado en https://popasmalinois.github.io/plmeco-docks/
-// Para Project Pages, la base debe ser '/<NOMBRE_REPO>/'.
+// Para Project Pages, la base debe ser '/<NOMBRE_REPO>/'
 export default defineConfig({
   plugins: [react()],
   base: '/plmeco-docks/',
